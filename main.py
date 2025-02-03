f89f2b3403f5787e453422ec31791f885a28a0da
Nehal <nehal.mohanlal12@gmail.com>
2025-02-03 19:11:26 +0200

    new gemma script

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from langchain_ollama import OllamaLLM 
 from langchain_core.prompts import ChatPromptTemplate 
 
+
 # template for prompt, needs more research on my part
 template = """
 Answer the question below.
@@ -16,7 +17,7 @@ Answer
 model_name = "gemma2:2b"
 
 #initialize the model
-model = OllamaLLM(model=model_name)
+model = OllamaLLM(model=model_name,streaming=True)
 
 #Structure the prompt. 
 prompt = ChatPromptTemplate.from_template(template) 
@@ -33,8 +34,9 @@ def handle_convo():
         user_input = input("you: ") 
         if user_input.lower() =="exit":
             break 
-        result = chain.invoke({"context":context, "question":user_input}) 
-        print("Bot: ", result) 
+        result = chain.invoke({"context":context, "question":user_input})
+         
+        print("Gemma: ", result) 
         
         ## store the conversation history
         context += f"\n User:{user_input} \n AI: {result}" 
